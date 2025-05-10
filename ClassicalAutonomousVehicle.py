674035406bf88f5f192346cb46a5810d4c56d2ee
aliyan16 <aliyanm12376@gmail.com>
2025-05-11 02:17:21 +0500

    lane lines detection

--- ClassicalAutonomousVehicle.py
+++ ClassicalAutonomousVehicle.py
@@ -61,6 +61,105 @@ def increase_contrast(img):
     return cv.addWeighted(img, 0.3, enhanced, 0.7, 0)
    
 
+def hough_transform(image):
+    """
+    Determine and cut the region of interest in the input image.
+    Parameter:
+        image: grayscale image which should be an output from the edge detector
+    """
+    # Distance resolution of the accumulator in pixels.
+    rho = 1             
+    # Angle resolution of the accumulator in radians.
+    theta = np.pi/180   
+    # Only lines that are greater than threshold will be returned.
+    threshold = 20      
+    # Line segments shorter than that are rejected.
+    minLineLength = 20  
+    # Maximum allowed gap between points on the same line to link them
+    maxLineGap = 500    
+    # function returns an array containing dimensions of straight lines 
+    # appearing in the input image
+    return cv.HoughLinesP(image, rho = rho, theta = theta, threshold = threshold,minLineLength = minLineLength, maxLineGap = maxLineGap)
+   
+def average_slope_intercept(lines):
+    """
+    Find the slope and intercept of the left and right lanes of each image.
+    Parameters:
+        lines: output from Hough Transform
+    """
+    left_lines    = [] #(slope, intercept)
+    left_weights  = [] #(length,)
+    right_lines   = [] #(slope, intercept)
+    right_weights = [] #(length,)
+     
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            if x1 == x2:
+                continue
+            # calculating slope of a line
+            slope = (y2 - y1) / (x2 - x1)
+            # calculating intercept of a line
+            intercept = y1 - (slope * x1)
+            # calculating length of a line
+            length = np.sqrt(((y2 - y1) ** 2) + ((x2 - x1) ** 2))
+            # slope of left lane is negative and for right lane slope is positive
+            if slope < 0:
+                left_lines.append((slope, intercept))
+                left_weights.append((length))
+            else:
+                right_lines.append((slope, intercept))
+                right_weights.append((length))
+    # 
+    left_lane  = np.dot(left_weights,  left_lines) / np.sum(left_weights)  if len(left_weights) > 0 else None
+    right_lane = np.dot(right_weights, right_lines) / np.sum(right_weights) if len(right_weights) > 0 else None
+    return left_lane, right_lane
+   
+def pixel_points(y1, y2, line):
+    """
+    Converts the slope and intercept of each line into pixel points.
+        Parameters:
+            y1: y-value of the line's starting point.
+            y2: y-value of the line's end point.
+            line: The slope and intercept of the line.
+    """
+    if line is None:
+        return None
+    slope, intercept = line
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    y1 = int(y1)
+    y2 = int(y2)
+    return ((x1, y1), (x2, y2))
+   
+def lane_lines(image, lines):
+    """
+    Create full lenght lines from pixel points.
+        Parameters:
+            image: The input test image.
+            lines: The output lines from Hough Transform.
+    """
+    left_lane, right_lane = average_slope_intercept(lines)
+    y1 = image.shape[0]
+    y2 = y1 * 0.6
+    left_line  = pixel_points(y1, y2, left_lane)
+    right_line = pixel_points(y1, y2, right_lane)
+    return left_line, right_line
+ 
+     
+def draw_lane_lines(image, lines, color=[255, 0, 0], thickness=12):
+    """
+    Draw lines onto the input image.
+        Parameters:
+            image: The input test image (video frame in our case).
+            lines: The output lines from Hough Transform.
+            color (Default = red): Line color.
+            thickness (Default = 12): Line thickness. 
+    """
+    line_image = np.zeros_like(image)
+    for line in lines:
+        if line is not None:
+            cv.line(line_image, *line,  color, thickness)
+    return cv.addWeighted(image, 1.0, line_image, 1.0, 0.0)
 
 if __name__=='__main__':
     imgPath = r'C:\AllData\Selfskills\MachineLearning&ComputerVision\DipProject\Autonomous-Vehicle-Classical-Image-Processing\Images\frame1.jpg'
@@ -83,4 +182,8 @@ if __name__=='__main__':
     cv.waitKey(2000)
     rocImg=region_of_interest(cannyImg)
     cv.imshow('roc img',rocImg)
+    cv.waitKey(2000)
+    HoughImg=hough_transform(rocImg)
+    result = draw_lane_lines(img, lane_lines(img, HoughImg))
+    cv.imshow('Lines',result)
     cv.waitKey(0)
