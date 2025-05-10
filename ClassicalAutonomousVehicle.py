ff8aae0792fdce2382ffd412b43dd5477d82d2cb
aliyan16 <aliyanm12376@gmail.com>
2025-05-11 01:15:04 +0500

    reducing noise

--- ClassicalAutonomousVehicle.py
+++ ClassicalAutonomousVehicle.py
@@ -13,7 +13,12 @@ def smoothing(img):
 def cannyEdge(img):
 
 
-    return cv.Canny(img,50,150)
+    return cv.Canny(img,80,200)
+def remove_noise(edges):
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+    opened = cv.morphologyEx(edges, cv.MORPH_OPEN, kernel)
+    return opened
+
 
 def preprocess_image(img):
     """Convert to grayscale and enhance contrast"""
@@ -38,6 +43,23 @@ def region_of_interest(img):
     ]], dtype=np.int32)
     cv.fillPoly(mask, polygon, 255)
     return cv.bitwise_and(img, mask)
+def increase_contrast(img):
+   """Best balance for lane detection"""
+   if len(img.shape) == 3:
+    lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+    l, a, b = cv.split(lab)
+        # Moderate CLAHE settings
+    clahe = cv.createCLAHE(clipLimit=1.0, tileGridSize=(16,16))
+    cl = clahe.apply(l)
+        # Blend 70% of enhanced with 30% original
+    blended_l = cv.addWeighted(l, 0.3, cl, 0.7, 0)
+    limg = cv.merge((blended_l,a,b))
+    return cv.cvtColor(limg, cv.COLOR_LAB2BGR)
+   else:
+    clahe = cv.createCLAHE(clipLimit=1.0, tileGridSize=(16,16))
+    enhanced = clahe.apply(img)
+    return cv.addWeighted(img, 0.3, enhanced, 0.7, 0)
+   
 
 
 if __name__=='__main__':
@@ -46,11 +68,15 @@ if __name__=='__main__':
     img=cv.resize(img,(512,512))
     cv.imshow('original img',img)
     cv.waitKey(2000)
-    enhanced = preprocess_image(img)
+    high_contrast_img = increase_contrast(img)
+    cv.imshow('high contrast img', high_contrast_img)
+    cv.waitKey(2000)
+    enhanced = preprocess_image(high_contrast_img)
     cv.imshow('preprocessed img',enhanced)
     cv.waitKey(2000)
     gaussianImg=smoothing(enhanced)
     cannyImg=cannyEdge(gaussianImg)
+    # CleancannyImg=remove_noise(cannyImg)
     # cannyImg=cv.resize(cannyImg,(512,512))
     cv.imshow('edge img',cannyImg)
     cv.waitKey(2000)
